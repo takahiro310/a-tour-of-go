b1b0f718103cd23df734ae25c725a0d3da23f114
takahiro310 <sato.takahiro0811@gmail.com>
2018-11-24 21:56:39 +0900

    Add images.go

---
+++ images.go
@@ -0,0 +1,22 @@
+package main
+
+import "golang.org/x/tour/pic"
+import "image"
+import "image/color"
+
+type Image struct{}
+
+func (img Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+func (img Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
+}
+func (img Image) At(x, y int) color.Color {
+	return color.RGBA{0, 0, 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
