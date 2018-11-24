657747ad179199f39f2cf974803a4f4c0871a2b3
takahiro310 <sato.takahiro0811@gmail.com>
2018-11-24 20:52:07 +0900

    Add readers.go

---
+++ readers.go
@@ -0,0 +1,19 @@
+package main
+
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
+func (r MyReader) Read(b []byte) (n int, err error) {
+
+	for i := 0; i < len(b); i++ {
+		b[i] = 'A'
+	}
+
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
